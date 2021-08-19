d1ea666317e3dfde1ce9a77387dfba54192b281a
Miroslav Vršecký <office@yamiteru.xyz>
2021-08-19 15:08:38 +0200

    Report template compilation time

--- src/server/utils/asyncHandler.ts
+++ src/server/utils/asyncHandler.ts
@@ -1,12 +1,22 @@
 import {HttpRequest, HttpResponse} from "uWebSockets.js";
 import {Route} from "../types";
 
+const { hrtime } = process;
+
 export const asyncHandler = <T>(route: Route<T>) =>
     async (res: HttpResponse, req: HttpRequest) => {
         res.onAborted(() => res.aborted = true);
 
+        const start = hrtime.bigint();
+
         const data = await route.data(req);
         const view = route.page(data)();
+
+        const end = hrtime.bigint();
+        const ns = end - start;
+        const ms = (Number(ns) / 1000000).toFixed(5);
+
+        console.log(`${req.getUrl()} -> ${ms} ms / ${ns} ns`);
 
         !res.aborted && res.cork(() => res
             .writeHeader("Access-Control-Allow-Origin", "*")
