f162569d1afe624a3d08c0a79c808a130bc0b0e3
galennedelchev <galen.nedelchev@rascasone.com>
2021-08-22 20:42:58 +0200

    Update test page spelling

--- src/test/pages/index.data.ts
+++ src/test/pages/index.data.ts
@@ -1,17 +1,17 @@
-import {Data} from "../../server/types";
+import { Data } from "../../server/types";
 
 export type IndexProps = {
-    name: string;
-    links: [title: string, link: string][];
+  name: string;
+  links: [title: string, link: string][];
 };
 
 const IndexData: Data<IndexProps> = async () => ({
-    name: "Yamiteru",
-    links: [
-        ["Susskind", "https://github.com/the-yamiteru/susskind"],
-        ["Stoic", "https://github.com/the-yamiteru/stoic"],
-        ["uWebSockets.js", "https://github.com/uNetworking/uWebSockets.js"],
-    ],
+  name: "Yamiteru",
+  links: [
+    ["Susskind", "https://github.com/the-yamiteru/susskind"],
+    ["Stoic", "https://github.com/the-yamiteru/stoic"],
+    ["uWebSockets.js", "https://github.com/uNetworking/uWebSockets.js"],
+  ],
 });
 
 export default IndexData;
--- src/test/pages/index.page.ts
+++ src/test/pages/index.page.ts
@@ -1,35 +1,25 @@
-import {IndexProps} from "./index.data";
+import { IndexProps } from "./index.data";
 import Page from "../components/Page";
-import {a, div, h1, li, title, ul} from "../../core/tags";
-import {For, Match, Show, Switch} from "../../core/flow";
-import {Component} from "../../core/types/common";
+import { A, Div, H1, Li, Title, Ul } from "../../core/tags";
+import { For, Match, Show, Switch } from "../../core/flow";
+import { Component } from "../../core/types/common";
 
 const IndexPage: Component<IndexProps> = ({ name, links }) =>
-    Page(
-        title()("Test - Homepage")
-    )(
-        h1()(
-            Switch(
-                Match(() => name === "Yamiteru")(
-                    "Welcome back master."
-                ),
-                Match(() => name === "Hacker")(
-                    "Noo please spare me!"
-                )
-            )(
-                `Hello ${name}!`
-            )
-        ),
-        ul()(
-            For(links)(([title, link], i) =>
-                li()(
-                    a({ href: link })(title),
-                    Show(() => (i + 1) % 2 == 0)(
-                        div()("EVEN")
-                    )
-                )
-            )
+  Page(Title()("Test - Homepage"))(
+    H1()(
+      Switch(
+        Match(() => name === "Yamiteru")("Welcome back master."),
+        Match(() => name === "Hacker")("Noo please spare me!")
+      )(`Hello ${name}!`)
+    ),
+    Ul()(
+      For(links)(([title, link], i) =>
+        Li()(
+          A({ href: link })(title),
+          Show(() => (i + 1) % 2 == 0)(Div()("EVEN"))
         )
-    );
+      )
+    )
+  );
 
 export default IndexPage;
