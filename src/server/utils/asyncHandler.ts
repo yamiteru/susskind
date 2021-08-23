a71ea1c09e6c0c3fd5da0a9bfcfcfcd8734fc716
Miroslav Vršecký <office@yamiteru.xyz>
2021-08-23 17:26:15 +0200

    Log multiple time metrics

--- src/server/utils/asyncHandler.ts
+++ src/server/utils/asyncHandler.ts
@@ -4,21 +4,30 @@ import {StoreGetters} from "../../core/types/common";
 
 const { hrtime } = process;
 
+const ms = (ns: BigInt) => (Number(ns) / 1000000).toFixed(5);
+
 export const asyncHandler = <I, S extends StoreGetters>(route: Route<I, S>) =>
     async (res: HttpResponse, req: HttpRequest) => {
         res.onAborted(() => res.aborted = true);
 
-        const start = hrtime.bigint();
-
+        const sData = hrtime.bigint();
         const data = await route.data(req);
+        const eData = hrtime.bigint();
+
+        const sStore = hrtime.bigint();
         const store = route.store(data);
+        const eStore = hrtime.bigint();
+
+        const sView = hrtime.bigint();
         const { view, type } = route.view(store);
+        const eView = hrtime.bigint();
 
-        const end = hrtime.bigint();
-        const ns = end - start;
-        const ms = (Number(ns) / 1000000).toFixed(5);
+        const tData = eData - sData;
+        const tStore = eStore - sStore;
+        const tView = eView - sView;
+        const tAll = tData + tStore + tView;
 
-        console.log(`${req.getUrl()} -> ${ms} ms / ${ns} ns`);
+        console.log(`${req.getUrl()} -> ${ms(tAll)}ms [${ms(tData)}, ${ms(tStore)}, ${ms(tView)}]`);
 
         !res.aborted && res.cork(() => res
             .writeHeader("Access-Control-Allow-Origin", "*")
